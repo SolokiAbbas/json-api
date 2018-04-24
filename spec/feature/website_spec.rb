07398a919a6b7daa8e66772fa99d233e8056ba3c
Abbas <SolokiAbbas@gmail.com>
2018-04-24 13:20:08 -0400

    added test to database

--- spec/feature/website_spec.rb
+++ spec/feature/website_spec.rb
@@ -1,6 +1,10 @@
 
 describe "Adding URLS to Database" do
+  subject(:websites){Website.new({url: "new", links: "new", h1: "new", h2: "new", h3: "new"})}
+
   it "should add to database" do
+    websites.save
+    expect(Website.last).to equal(websites)
   end
 
   it "should add multiple tags" do
