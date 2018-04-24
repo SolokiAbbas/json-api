df370d77cb4f8764875bc53ee6c376412b3448dd
Abbas <SolokiAbbas@gmail.com>
2018-04-23 23:39:42 -0400

    added index and create for api

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -1,12 +1,20 @@
 class WebsitesController < ApplicationController
 
   def index
+    @websites = Websites.all
+    render :index
   end
 
   def create
+    @websites = Websites.new
+    if @websites.save
+      render :index
+    else
+      ender json: @websites.errors.full_messages, status: 422
+    end
   end
 
   def show
   end
-  
+
 end
