fd7b3d79c78a2fdf3cad8742bc26d08d0f62355f
Abbas <SolokiAbbas@gmail.com>
2018-04-24 13:12:23 -0400

    added tests with rspec

---
+++ spec/feature/website_spec.rb
@@ -0,0 +1,25 @@
+
+describe "Adding URLS to Database" do
+  it "should add to database" do
+  end
+
+  it "should add multiple tags" do
+  end
+
+  it "should display all in database" do
+  end
+
+end
+
+describe "Testing JSON URLS", type: :feature do
+
+  it "should get a link and add it to database" do
+  end
+
+  it "should should make sure its a link" do
+  end
+
+  it "should display all in database" do
+  end
+
+end
